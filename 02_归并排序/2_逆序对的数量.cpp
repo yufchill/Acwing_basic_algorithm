cfa1ec06dd50e5ceb4c2fa69cad8b8164e2989c4
陈宇希 <cyx_chenyuxi@163.com>
2025-06-24 10:33:43 +0800

    finish merge_sort

---
+++ 02_归并排序/2_逆序对的数量.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long  ans = 0;
+void merge_sort(vector<int> &nums, int left, int right) {
+  if (left >= right) return;
+  int mid = (right - left) / 2 + left;
+  merge_sort(nums, left, mid);
+  merge_sort(nums, mid + 1, right);
+  int i = left, j = mid + 1;
+  vector<int> temp(right - left + 1);
+  int idx = 0;
+  while (i <= mid && j <= right) {
+    if (nums[i] <= nums[j]) {
+      temp[idx++] = nums[i++];
+    } else {
+      ans += (mid - i + 1);
+      temp[idx++] = nums[j++];
+    }
+  }
+  while (i <= mid) {
+    temp[idx++] = nums[i++];
+  }
+  while (j <= right) {
+    temp[idx++] = nums[j++];
+  }
+  for (int i = left, idx = 0; i <= right; i++) {
+    nums[i] = temp[idx++];
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> nums(n);
+  for (int i = 0; i < n; i++) {
+    cin >> nums[i];
+  }
+  merge_sort(nums, 0, nums.size() - 1);
+  cout << ans << endl;
+}
